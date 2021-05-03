bb0623cb7bf5f19f631331a45a6b8ddadd4be1a6
hermanke <hermanke@tencent.com>
2021-05-03 10:02:19 +0800

    feat: study

---
+++ binaryTree/814.二叉树剪枝.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=814 lang=javascript
+ *
+ * [814] 二叉树剪枝
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ * 递归
+ * 判断左右节点是否为空，且当前节点的值为0
+ */
+var pruneTree = function(root) {
+  if(!root) return null;
+  root.left = pruneTree(root.left);
+  root.right = pruneTree(root.right);
+  if(root.val === 0 && root.left == null && root.right == null){
+    // 符合裁剪节点
+    return null;
+  }
+  return root;
+};
+// @lc code=end
+
