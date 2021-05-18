7b0504ae121ae699986b53e23c22ae14942c0197
hermanke <hermanke@tencent.com>
2021-05-18 12:00:10 +0800

    feat: study

---
+++ array/1.两数之和.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=javascript
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ * 构建map解法
+ */
+var twoSum = function(nums, target) {
+  const map = new Map();
+  const len = nums.length;
+  for(let i=0;i<len;i++){
+    const num = nums[i];
+    const n = target - num;
+    if(map.has(n)){
+      return [map.get(n),i];
+    }else{
+      map.set(num, i);
+    }
+  }
+  return [];
+};
+// @lc code=end
+
